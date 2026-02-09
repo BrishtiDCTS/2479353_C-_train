6e5edcfe3e6afff5a9c625bb9bf6a0644935c8b8
BrishtiDCTS <Brishti.Das@cognizant.com>
2026-02-09 18:53:19 +0000

    Its day 1 technical

---
+++ 1_firstcsharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UserInfoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+    
+            Console.Write("Enter your salary: ");
+            double salary = Convert.ToDouble(Console.ReadLine());
+
+       
+            Console.WriteLine($"Hello {name}! You are {age} years old and your salary is {salary:C}.");
+            
+            
+        }
+    }
+}
---
+++ 2_PositiveNegetive/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NumberChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+
+            int number = Convert.ToInt32(input);
+
+            if (number > 0)
+            {
+                Console.WriteLine($"{number} is a Positive number.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"{number} is a Negative number.");
+            }
+            else
+            {
+                Console.WriteLine("The number is Zero.");
+            }
+        }
+    }
+}
